b80201d2a16f059fb0290de33ab97893f0fe0feb
Andrea <andrea.spina@radicalbit.io>
2021-04-22 14:14:36 +0200

    introducing flyway, base table, logging, containerization (#3)

--- src/main/scala/org/spixi/http4splayground/Main.scala
+++ src/main/scala/org/spixi/http4splayground/Main.scala
@@ -4,6 +4,5 @@ import cats.effect.{ExitCode, IO, IOApp}
 
 object Main extends IOApp {
 
-  def run(args: List[String]): IO[ExitCode] =
-    Server.stream[IO].compile.drain.as(ExitCode.Success)
+  def run(args: List[String]): IO[ExitCode] = Server.stream[IO].compile.drain.as(ExitCode.Success)
 }
--- src/main/scala/org/spixi/http4splayground/Server.scala
+++ src/main/scala/org/spixi/http4splayground/Server.scala
@@ -1,27 +1,40 @@
 package org.spixi.http4splayground
 
 import cats.effect.{ConcurrentEffect, ContextShift, Timer}
+import cats.implicits._
+import doobie.util.ExecutionContexts
 import fs2.Stream
+import io.circe.config.parser
 import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
-import org.http4s.server.middleware.Logger
+import org.http4s.server.middleware.{Logger => MLogger}
+import org.slf4j.{Logger, LoggerFactory}
 import org.spixi.http4splayground.clients.SumClient
+import org.spixi.http4splayground.config.{DatabaseConfig, Http4sPlaygroundConfig}
 import org.spixi.http4splayground.services.CalculatorService
 
-import scala.concurrent.ExecutionContext.global
+import scala.concurrent.duration._
 
 object Server {
 
+  implicit val log: Logger = LoggerFactory.getLogger(this.getClass)
+
   def stream[F[_]: ConcurrentEffect](implicit T: Timer[F], cs: ContextShift[F]): Stream[F, Nothing] = {
     for {
+      // logger and configs
+      conf <- Stream.eval(parser.decodePathF[F, Http4sPlaygroundConfig]("application"))
+      // execution contexts
+      serverEc <- Stream.resource(ExecutionContexts.cachedThreadPool[F])
+      clientEc <- Stream.resource(ExecutionContexts.cachedThreadPool[F])
+      // connEc <- Stream.resource(ExecutionContexts.fixedThreadPool[F](conf.database.connections.poolSize))
+      // txnEc <- Stream.resource(ExecutionContexts.cachedThreadPool[F])
       // clients
-      _ <- BlazeClientBuilder[F](global).stream
+      _ <- BlazeClientBuilder[F](clientEc).stream
       sumClient = SumClient.impl[F]
 
       // services
       sumSerivceImpl = CalculatorService.impl[F](sumClient)
-
       // Combine Service Routes into an HttpApp.
       // Can also be done via a Router if you
       // want to extract a segments not checked
@@ -31,12 +44,15 @@ object Server {
       ).orNotFound
 
       // With Middlewares in place
-      finalHttpApp = Logger.httpApp(logHeaders = true, logBody = true)(httpApp)
-
-      exitCode <- BlazeServerBuilder[F](global)
-        .bindHttp(8080, "0.0.0.0")
+      finalHttpApp = MLogger.httpApp(logHeaders = true, logBody = true)(httpApp)
+      migrationF   = DatabaseConfig.initializeDb(conf.database, conf.flyway)
+      _ <- Stream
+        .retry(migrationF, 1.second, nextDelay = _ * 2, 5)
+        .handleError { e => log.error(s"Failed to provide migration init.", e); () }
+      exitCode <- BlazeServerBuilder[F](serverEc)
+        .bindHttp(conf.server.port, conf.server.host)
         .withHttpApp(finalHttpApp)
-        .serve
+        .stream
     } yield exitCode
   }.drain
 }
