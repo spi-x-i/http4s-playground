6d28b335ffe774aca26f9d368331be33e9704309
Andrea Spina <andrea.spina@radicalbit.io>
2021-04-26 23:59:44 +0200

    implementing user repository, manual tested

--- src/main/scala/org/spixi/http4splayground/Routes.scala
+++ src/main/scala/org/spixi/http4splayground/Routes.scala
@@ -5,7 +5,8 @@ import cats.implicits._
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 import org.http4s.dsl.impl.OptionalQueryParamDecoderMatcher
-import org.spixi.http4splayground.services.CalculatorService
+import org.spixi.http4splayground.models.UserInputDTO
+import org.spixi.http4splayground.services.{CalculatorService, UserService}
 
 object Routes {
 
@@ -19,6 +20,18 @@ object Routes {
       for {
         result   <- H.sum(add1.getOrElse(0), add2.getOrElse(0))
         response <- Ok(result)
+      } yield response
+    }
+  }
+
+  def userControllerRoutes[F[_]: Sync](userService: UserService[F]): HttpRoutes[F] = {
+    val dsl = new Http4sDsl[F] {}
+    import dsl._
+    HttpRoutes.of[F] { case req @ POST -> Root / "users" =>
+      for {
+        userIn      <- req.as[UserInputDTO]
+        userCreated <- userService.create(userIn)
+        response    <- Ok(userCreated)
       } yield response
     }
   }
