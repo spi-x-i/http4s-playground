7629804d7afcd934b7e136ad4e8b77724485f85a
Francesco Frontera <fra.frontera01@gmail.com>
2021-04-27 11:03:41 +0200

    refactoring: remove implicit imports (#5)

--- src/main/scala/org/spixi/http4splayground/services/UserService.scala
+++ src/main/scala/org/spixi/http4splayground/services/UserService.scala
@@ -1,29 +1,33 @@
 package org.spixi.http4splayground.services
 
-import cats.Monad
-import cats.effect.Timer
-import cats.implicits._
+import cats.effect._
+import cats.syntax.all._
 import org.slf4j.Logger
 import org.spixi.http4splayground.dao.UserDAO
 import org.spixi.http4splayground.dao.table.UserTable
 import org.spixi.http4splayground.models.{UserInputDTO, UserOutputDTO}
+import cats.Monad
 
 trait UserService[F[_]] {
-  def create(userIn: UserInputDTO)(implicit M: Monad[F]): F[UserOutputDTO]
+  def create(userIn: UserInputDTO): F[UserOutputDTO]
 }
 
 object UserService {
   implicit def apply[F[_]](implicit ev: UserService[F]): UserService[F] = ev
 
-  def impl[F[_]](userDAO: UserDAO[F])(implicit log: Logger, T: Timer[F]): UserService[F] =
+  def impl[F[_]](userDAO: UserDAO[F])(implicit
+      log: Logger,
+      T: Timer[F],
+      M: Monad[F]
+  ): UserService[F] =
     new UserService[F] {
 
-      def create(userIn: UserInputDTO)(implicit M: Monad[F]): F[UserOutputDTO] = {
+      def create(userIn: UserInputDTO): F[UserOutputDTO] = {
         log.info(s"User creation request: user: $userIn.")
         for {
           userToCreate <- UserTable.fromUserInput(userIn)
           userCreated  <- userDAO.create(userToCreate)
-          result       <- UserOutputDTO.fromUser(userCreated)(M)
+          result       <- UserOutputDTO.fromUser(userCreated)
         } yield result
       }
     }
